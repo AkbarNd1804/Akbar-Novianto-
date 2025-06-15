c9978c456e24cca0e972ce44a0f1255f01b1822b
akbar novianto danarhadi <akbarnoviantodanarhadi@gmail.com>
2025-06-15 21:04:39 +0700

    menyiapkan proyek latihan 1

---
+++ hitung/src/hitung/Hitung.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitung;
+
+/**
+ *
+ * @author DESI
+ */
+
+public class Hitung {
+    private double bil1;
+    private double bil2;
+
+    // Constructor
+    public Hitung (double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+
+    public double setPenjumlahan() {
+        return bil1 + bil2;
+    }
+}
+
+        // TODO code application logic here   
