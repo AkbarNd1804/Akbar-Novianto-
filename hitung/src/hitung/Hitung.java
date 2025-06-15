d982ea3c8ca09e4903a967391f8f9a6f570ddd2b
akbar novianto danarhadi <akbarnoviantodanarhadi@gmail.com>
2025-06-15 21:10:36 +0700

    melengkapi method matematika

--- hitung/src/hitung/Hitung.java
+++ hitung/src/hitung/Hitung.java
@@ -22,7 +22,23 @@ public class Hitung {
 
     public double setPenjumlahan() {
         return bil1 + bil2;
+    }  
+    public double setPengurangan() {
+        return bil1 - bil2;
     }
-}
+
+    public double setPerkalian() {
+        return bil1 * bil2;
+    }
+
+    public double setPembagian() {
+        if (bil2 != 0) {
+            return bil1 / bil2;
+        } else {
+            System.out.println("Error: Pembagian dengan nol!");
+            return 0;
+        }
+    }
+    }
 
         // TODO code application logic here   
